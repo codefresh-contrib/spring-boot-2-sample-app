988a0d256a8c09ad68b859009bb6ac8a38fc9923
Kostis Kapelonis <kostis@codefresh.io>
2018-06-11 07:59:17 -0400

    Added rest assured integration tests

--- src/test/java/sample/actuator/HealthIT.java
+++ src/test/java/sample/actuator/HealthIT.java
@@ -2,11 +2,36 @@ package sample.actuator;
 
 import org.junit.Test;
 
+import static io.restassured.RestAssured.given;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.hamcrest.Matchers.equalTo;
+
 public class HealthIT {
 	
 	@Test
-	public void healthCheck() {
-		
-	}
+    public void running() {
+		given().when().get("http://localhost:8080/")
+            .then().statusCode(200);
+    }
+	
+	@Test
+    public void message() {
+		given().when().get("http://localhost:8080/")
+            .then().body(containsString("Spring boot"));
+    }
+	
+	@Test
+    public void fullMessage() {
+		given().when().get("http://localhost:8080/")
+            .then().body("message",equalTo("Spring boot says hello from a Docker container"));
+    }
+	
+	@Test
+    public void health() {
+		given().when().get("http://localhost:8080/actuator/health")
+            .then().body("status",equalTo("UP"));
+    }
+
 
 }
