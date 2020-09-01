212a6a183b90592f9e12d546e2708a4af680fd26
Dustin Van Buskirk <dev@vanbuskirk.me>
2020-09-01 15:32:34 -0700

    removing debug lines

--- ec2-create-provision.tf
+++ ec2-create-provision.tf
@@ -101,8 +101,6 @@ resource "aws_eip" "aws_cf_tf" {
   # run jar
   provisioner "remote-exec" {
     inline = [
-      "echo ${aws_eip.aws_cf_tf.public_ip} > myip.txt",
-      "sleep 10",
       "nohup java -Djava.security.egd=file:/dev/./urandom -Dserver.port=8080 -Dserver.host=http://${aws_eip.aws_cf_tf.public_ip} -jar /home/ubuntu/spring-boot-application.jar &; disown"
     ]
   }
