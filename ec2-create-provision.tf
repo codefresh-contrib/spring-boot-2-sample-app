0867304f3c86f22b3ec1de1ed5a0591ae79a059c
Dustin Van Buskirk <dev@vanbuskirk.me>
2020-09-01 10:06:28 -0700

    fixing issue

--- ec2-create-provision.tf
+++ ec2-create-provision.tf
@@ -72,7 +72,7 @@ resource "aws_eip" "aws_cf_tf" {
     user = "ubuntu"
     type = "ssh"
     private_key = "${file(var.private_key_path)}"
-    host = "aws_eip.aws_cf_tf.public_ip"
+    host = aws_eip.aws_cf_tf.public_ip
     # The connection will use the local SSH agent for authentication.
   } 
 
